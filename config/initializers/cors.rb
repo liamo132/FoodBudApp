0bf42c9b41b930b42442d30997cbe033798998a4
liamo132 <liamf.connell@gmail.com>
2025-12-07 01:43:37 +0000

    updated cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,8 +1,9 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'  # for project demo; in a real system you’d restrict this
-    resource '*',
+    origins "https://foodbud-backend.onrender.com", "http://localhost:5173"
+
+    resource "*",
       headers: :any,
-      methods: %i[get post put patch delete options head]
+      methods: [:get, :post, :patch, :put, :delete, :options, :head]
   end
 end
