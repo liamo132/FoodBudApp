4445fed1dfd862123bec86cdee4b55e5b1bd244a
liamo132 <liamf.connell@gmail.com>
2025-12-08 20:49:42 +0000

    Tests fix

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -7,4 +7,7 @@ class Restaurant < ApplicationRecord
   # Scope for user-added restaurants
   scope :user_added, -> { where(user_added: true) }
   scope :searched, -> { where(user_added: false) }
+
+  # Geocoder adds distance when using .near; provide a default nil to avoid errors when absent
+  attr_accessor :distance
 end
